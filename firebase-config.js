7ec7e543a7d77bec05a60701c3bb96730b6dc6f4
MohiuddinCo22 <161435524+MohiuddinCo22@users.noreply.github.com>
2026-08-25 21:29:46 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@
 // থেকে এই মানগুলো কপি করে আনবেন। বিস্তারিত README.md ফাইলে আছে।
 // ============================================================
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyBd8SELqb-VW8qhv5ikzcJ6EBWo8JTBC-w",
+  authDomain: "milad-mahfil-hishab.firebaseapp.com",
+  projectId: "milad-mahfil-hishab",
+  storageBucket: "milad-mahfil-hishab.firebasestorage.app",
+  messagingSenderId: "551678532386",
+  appId: "1:551678532386:web:52d82c9ad1ae65f816237b"
 };
 
 firebase.initializeApp(firebaseConfig);
